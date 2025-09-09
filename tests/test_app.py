07b8607e26f0eb3756709c6b80d1e4641c06d810
Pedro Dutra <pedrodutra86@hotmail.com>
2025-09-09 17:49:29 -0300

    feat: add home page with a message and test it, improve base endpoint with status code and pydantic response model

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,10 +5,33 @@ from fastapi.testclient import TestClient
 from todo_list.app import app
 
 
-def test_root_should_return_hello_world_and_return_200():
+def test_root_returns_200():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_root_returns_hello_world():
     client = TestClient(app)
 
     response = client.get('/')
 
     assert response.json() == {'message': 'Hello World!'}
+
+
+def test_home_page_return_200():
+    client = TestClient(app)
+
+    response = client.get('/home')
+
     assert response.status_code == HTTPStatus.OK
+
+
+def test_home_page_has_hello_world_text():
+    client = TestClient(app)
+
+    response = client.get('/home')
+
+    assert 'Hello World!' in response.text
--- todo_list/app.py
+++ todo_list/app.py
@@ -1,8 +1,29 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from todo_list.schemas import Message
+
+app = FastAPI(title='ToDo List')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello World!'}
+
+
+@app.get('/home', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def hello_world():
+    return """
+    <html>
+        <head>
+            <title>Hello World from FastAPI</title>
+        </head>
+        <body>
+            <h1>
+                Hello World!
+            </h1>
+        </body>
+    </html>
+    """
