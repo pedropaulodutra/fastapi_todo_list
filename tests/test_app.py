c966f79e4cf431e8be5f18c9e17f88bdf207c201
Pedro Dutra <pedrodutra86@hotmail.com>
2025-09-13 18:05:31 -0300

    feat: implement CRUD routs

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,37 +1,113 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from todo_list.app import app
-
-
-def test_root_returns_200():
-    client = TestClient(app)
-
+def test_root_returns_200(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
 
 
-def test_root_returns_hello_world():
-    client = TestClient(app)
-
+def test_root_returns_hello_world(client):
     response = client.get('/')
 
     assert response.json() == {'message': 'Hello World!'}
 
 
-def test_home_page_return_200():
-    client = TestClient(app)
-
+def test_home_page_return_200(client):
     response = client.get('/home')
 
     assert response.status_code == HTTPStatus.OK
 
 
-def test_home_page_has_hello_world_text():
-    client = TestClient(app)
-
+def test_home_page_has_hello_world_text(client):
     response = client.get('/home')
 
     assert 'Hello World!' in response.text
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'alice_secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [{'username': 'alice', 'email': 'alice@example.com'}]
+    }
+
+
+def test_read_user(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+    }
+
+
+def test_read_user_not_found(client):
+    response = client.get('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found.'}
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'bob_secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+    }
+
+
+def test_update_user_not_found(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'alice_secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found.'}
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'username': 'bob', 'email': 'bob@example.com'}
+
+
+def test_delete_user_not_found(client):
+    response = client.delete('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found.'}
--- todo_list/schemas.py
+++ todo_list/schemas.py
@@ -1,5 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class Message(BaseModel):
     message: str
+
+
+class UserPublic(BaseModel):
+    username: str
+    email: EmailStr
+
+
+class UserSchema(UserPublic):
+    password: str
+
+
+class UserDB(UserSchema):
+    id: int
+
+
+class UserList(BaseModel):
+    users: list[UserPublic]
